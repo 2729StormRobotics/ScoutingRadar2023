737cfb26550c34ae7a96b2c9c02f891e41fbbed4
Josh Wright <653522@lrstudents.org>
2022-01-27 16:22:36 -0500

    initial commit

--- app/src/main/java/org/stormrobotics/scoutingradar2022/MainActivity.java
+++ app/src/main/java/org/stormrobotics/scoutingradar2022/MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int ACCESS_LOCATION_REQUEST = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
     }
 }
