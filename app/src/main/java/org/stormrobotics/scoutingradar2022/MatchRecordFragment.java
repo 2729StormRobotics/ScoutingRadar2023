6a8f53f8c177b8cf6548698736a1aad0a885cac2
Sid Parikh <parikhsid16@gmail.com>
2022-01-29 21:58:28 -0500

    Finished setting up RecyclerView

--- app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordFragment.java
+++ app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,49 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.stormrobotics.scoutingradar2022.placeholder.PlaceholderContent;
-
 /**
  * A fragment representing a list of Items.
  */
 public class MatchRecordFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
-    private MatchViewModel mMatchViewModel;
-    public static final int NEW_MATCH_ACTIVITY_REQUEST_CODE = 1;
-
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
-    public MatchRecordFragment() {
-    }
+    public MatchRecordFragment() {}
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static MatchRecordFragment newInstance(int columnCount) {
-        MatchRecordFragment fragment = new MatchRecordFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
+    public static MatchRecordFragment newInstance() {
+        return new MatchRecordFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
-
-        mMatchViewModel = new ViewModelProvider(this).get(MatchViewModel.class);
-        mMatchViewModel.getDataList();
-
     }
 
     @Override
@@ -64,17 +38,20 @@ public class MatchRecordFragment extends Fragment {
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_match_record_list, container, false);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MatchRecordRecyclerViewAdapter());
-        }
+        // Get the recycler view
+        RecyclerView recyclerView = view.findViewById(R.id.record_recycler);
+        // Create the adapter
+        final MatchRecordRecyclerViewAdapter adapter = new MatchRecordRecyclerViewAdapter(new MatchRecordRecyclerViewAdapter.RecordDiff());
+        // Set up the recycler view with the adapter
+        Context context = view.getContext();
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+
+        // Create the ViewModel
+        MatchViewModel mMatchViewModel = new ViewModelProvider(this).get(MatchViewModel.class);
+
+        // Connect the viewModel and thus the database with the adapter and thus the recycler view
+        mMatchViewModel.getDataList().observe(getViewLifecycleOwner(), adapter::submitList);
         return view;
     }
 }
--- app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordRecyclerViewAdapter.java
+++ app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordRecyclerViewAdapter.java
@@ -1,5 +1,8 @@
 package org.stormrobotics.scoutingradar2022;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -17,41 +20,33 @@ import java.util.List;
  * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
  * TODO: Replace the implementation with code for your data type.
  */
-public class MatchRecordRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecordRecyclerViewAdapter.MatchRecordViewHolder> {
-
-    private final List<ObjectiveMatchData> mValues;
-
-    public MatchRecordRecyclerViewAdapter() { mValues = new ArrayList<>(); }
-
-    public MatchRecordRecyclerViewAdapter(List<ObjectiveMatchData> items) {
-        mValues = items;
+public class MatchRecordRecyclerViewAdapter extends ListAdapter<ObjectiveMatchData, MatchRecordRecyclerViewAdapter.MatchRecordViewHolder> {
+    protected MatchRecordRecyclerViewAdapter(
+            @NonNull
+                    DiffUtil.ItemCallback<ObjectiveMatchData> diffCallback) {
+        super(diffCallback);
     }
 
+    @NonNull
     @Override
-    public MatchRecordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+    public MatchRecordViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent, int viewType) {
         return new MatchRecordViewHolder(
                 FragmentMatchRecordBinding.inflate(LayoutInflater.from(parent.getContext()), parent,
                         false));
-
     }
 
     @Override
-    public void onBindViewHolder(final MatchRecordViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.mMatchNumView.setText(mValues.get(position).getMatchNum());
-        holder.mTeamNumView.setText(mValues.get(position).getTeamNum());
+    public void onBindViewHolder(
+            @NonNull MatchRecordViewHolder holder, int position) {
+        holder.bind(getItem(position));
     }
 
-    @Override
-    public int getItemCount() {
-        return mValues.size();
-    }
 
-    public class MatchRecordViewHolder extends RecyclerView.ViewHolder {
+    public static class MatchRecordViewHolder extends RecyclerView.ViewHolder {
         public final TextView mMatchNumView;
         public final TextView mTeamNumView;
-        public ObjectiveMatchData mItem;
+        ObjectiveMatchData mItem;
 
         public MatchRecordViewHolder(FragmentMatchRecordBinding binding) {
             super(binding.getRoot());
@@ -59,9 +54,29 @@ public class MatchRecordRecyclerViewAdapter extends RecyclerView.Adapter<MatchRe
             mTeamNumView = binding.recordTextTeamNumber;
         }
 
+        public void bind(ObjectiveMatchData item) {
+            mItem = item;
+            mMatchNumView.setText(item.getMatchNum());
+            mTeamNumView.setText(item.getTeamNum());
+        }
+
         @Override
         public String toString() {
             return super.toString() + " '" + mTeamNumView.getText() + "'";
+        }
+    }
+
+    public static class RecordDiff extends DiffUtil.ItemCallback<ObjectiveMatchData> {
+        @Override
+        public boolean areItemsTheSame(
+                @NonNull ObjectiveMatchData oldItem, @NonNull ObjectiveMatchData newItem) {
+            return false;
+        }
+
+        @Override
+        public boolean areContentsTheSame(
+                @NonNull ObjectiveMatchData oldItem, @NonNull ObjectiveMatchData newItem) {
+            return false;
         }
     }
 }
