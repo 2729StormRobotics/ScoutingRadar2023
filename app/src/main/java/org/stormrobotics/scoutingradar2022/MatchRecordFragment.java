4af89391f6313baa4372943f171e5bc0e483f8a5
Mohit Ambe <mohitambe77@gmail.com>
2022-01-31 16:47:20 -0500

    Added new fragments to UI + transitioning(not working yet)

--- app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordFragment.java
+++ app/src/main/java/org/stormrobotics/scoutingradar2022/MatchRecordFragment.java
@@ -5,17 +5,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBindings;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+import com.leinardi.android.speeddial.SpeedDialView;
+
 /**
  * A fragment representing a list of Items.
  */
 public class MatchRecordFragment extends Fragment {
+
+    NavController mNavController;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -52,6 +60,27 @@ public class MatchRecordFragment extends Fragment {
 
         // Connect the viewModel and thus the database with the adapter and thus the recycler view
         mMatchViewModel.getDataList().observe(getViewLifecycleOwner(), adapter::submitList);
+
+
+        // Setting up SpeedDialView
+        SpeedDialView speedDialView = view.findViewById(R.id.record_fab);
+        speedDialView.inflate(R.menu.menu_speed_dial);
+        speedDialView.setOnActionSelectedListener(this::onActionSelected);
+
+        mNavController = Navigation.findNavController(view);
+
         return view;
+    }
+    public boolean onActionSelected(SpeedDialActionItem actionItem) {
+
+        if (actionItem.getId() == R.id.item_pitscout) {
+            mNavController.navigate(R.id.action_matchRecordFragment_to_pitScoutFragment);
+        } else if (actionItem.getId() == R.id.item_subjective) {
+            mNavController.navigate(R.id.action_matchRecordFragment_to_subjectiveMatchFragment);
+        } else if (actionItem.getId() == R.id.item_objective) {
+            mNavController.navigate(R.id.action_matchRecordFragment_to_objectiveMatchFragment);
+        }
+
+        return false;
     }
 }
