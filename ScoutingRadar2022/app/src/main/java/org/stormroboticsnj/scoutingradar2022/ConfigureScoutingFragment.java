59585a819db65b1d81bfda5916f2b1f51043312f
NeonGems <neongamers65@gmail.com>
2023-02-05 16:51:02 -0500

    Revert "Merge branch 'subjective_scouting' of https://github.com/2729StormRobotics/ScoutingRadar2023 into subjective_scouting"
    This reverts commit cc3632a8c78b555a9ca9637dbf322dd18e1766a4, reversing
    changes made to 853843db37fe354572570591472861588951ecac.

--- ScoutingRadar2022/app/src/main/java/org/stormroboticsnj/scoutingradar2022/ConfigureScoutingFragment.java
+++ ScoutingRadar2022/app/src/main/java/org/stormroboticsnj/scoutingradar2022/ConfigureScoutingFragment.java
@@ -44,8 +44,6 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
     private String mObjButtons = "";
     private String mObjAbbreviations = "";
     private String mObjSpinners = "";
-    private String mSubButtons = "";
-    private String mSubAbbreviations = "";
     private String mSubSpinners = "";
     private String mPitSpinners = "";
 
@@ -135,9 +133,6 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
         mCurrentPrefsTextView.append("\n" + mObjAbbreviations + "\n");
         mCurrentPrefsTextView.append("\nObjective Spinners\n");
         mCurrentPrefsTextView.append(mObjSpinners);
-        mCurrentPrefsTextView.append("\nSubjective Buttons\n");
-        mCurrentPrefsTextView.append(mSubButtons);
-        mCurrentPrefsTextView.append("\n" + mSubAbbreviations + "\n");
         mCurrentPrefsTextView.append("\nSubjective Spinners\n");
         mCurrentPrefsTextView.append(mSubSpinners);
         mCurrentPrefsTextView.append("\nPit Spinners\n");
@@ -183,9 +178,7 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
                 Set<String> pitSpinners = new HashSet<>();
                 Set<String> objSpinners = new HashSet<>();
                 String objButtons = "";
-                String subButtons = "";
                 String objAbbreviations = "";
-                String subAbbreviations = "";
 
                 // This warning is because Ini implements Map and the linter doesn't realize that
                 // the constructor builds data
@@ -248,17 +241,7 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
 
                     objAbbreviations = section.get("Abbreviations");
                 }
-                // Subjective Buttons
-                section = ini.get("subjective_buttons");
-                if (section != null) {
-                    // Given format:
-                    // key:value is [button name]:[button abbreviation]
-                    // But this needs to be ordered
 
-                    subButtons = section.get("Names");
-
-                    subAbbreviations = section.get("Abbreviations");
-                }
                 SharedPreferences.Editor e = mSharedPreferences.edit();
                 e.putStringSet(getString(R.string.pref_key_sub_spinner), subSpinners)
                  .putStringSet(getString(R.string.pref_key_pit_spinner), pitSpinners)
@@ -268,16 +251,8 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
                     e.putString(getString(R.string.pref_key_obj_abbrs), objAbbreviations);
                 }
 
-                if (subAbbreviations != null) {
-                    e.putString(getString(R.string.pref_key_sub_abbrs), subAbbreviations);
-                }
-
                 if (objButtons != null) {
                     e.putString(getString(R.string.pref_key_obj_buttons), objButtons);
-                }
-
-                if (subButtons != null) {
-                    e.putString(getString(R.string.pref_key_sub_buttons), subButtons);
                 }
 
                 e.apply();
@@ -302,14 +277,6 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
             mObjAbbreviations = formatPrefString(
                     mSharedPreferences.getString(getString(R.string.pref_key_obj_abbrs),
                             getString(R.string.obj_abbrs_default)));
-        }  else if (getString(R.string.pref_key_sub_buttons).equals(key)) {
-        mSubButtons = formatPrefString(
-                mSharedPreferences.getString(getString(R.string.pref_key_sub_buttons),
-                        getString(R.string.obj_buttons_default)));
-    } else if (getString(R.string.pref_key_sub_abbrs).equals(key)) {
-            mSubAbbreviations = formatPrefString(
-                    mSharedPreferences.getString(getString(R.string.pref_key_sub_abbrs),
-                            getString(R.string.obj_abbrs_default)));
         } else if (getString(R.string.pref_key_obj_spinner).equals(key)) {
             mObjSpinners =
                     formatArray(readPrefs(R.string.pref_key_obj_spinner, R.array.obj_spinners));
@@ -320,7 +287,6 @@ public class ConfigureScoutingFragment extends Fragment implements SharedPrefere
             mPitSpinners =
                     formatArray(readPrefs(R.string.pref_key_pit_spinner, R.array.pit_spinners));
         }
-
 
         updateConfigTextView();
     }
--- ScoutingRadar2022/app/src/main/java/org/stormroboticsnj/scoutingradar2022/database/subjective/ChangeBackgroundByButtonClick.java
+++ ScoutingRadar2022/app/src/main/java/org/stormroboticsnj/scoutingradar2022/database/subjective/ChangeBackgroundByButtonClick.java
@@ -1,38 +1,22 @@
 package org.stormroboticsnj.scoutingradar2022.database.subjective;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ScrollView;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
 
 import org.stormroboticsnj.scoutingradar2022.R;
 
 public class ChangeBackgroundByButtonClick extends AppCompatActivity {
-
-
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Button subjective_button_red, subjective_button_blue;
-        ScrollView layout;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_subjective_match);
-
-        subjective_button_red = findViewById(R.id.subjective_button_red);
-        subjective_button_blue = findViewById(R.id.subjective_button_blue);
-        layout = findViewById(R.id.scrollview);
-
-        subjective_button_red.setOnClickListener(view -> layout.setBackgroundColor(Color.RED));
-
-        subjective_button_blue.setOnClickListener(view -> layout.setBackgroundColor(Color.BLUE));
-
+        view = this.getWindow().getDecorView();
     }
 
-
-
-
-
+    public void goRed(View v){
+        view.setBackgroundResource(R.color.backgroundRed);
+    }
 }
